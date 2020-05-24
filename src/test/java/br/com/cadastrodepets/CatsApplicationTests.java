9af88af55342f75d823738e2ea64f1502291c6a5
fndribeiro <fndribeiro@hotmail.com>
2020-05-24 00:16:09 -0300

    projeto inicial.

---
+++ src/test/java/br/com/cadastrodepets/CatsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastrodepets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
