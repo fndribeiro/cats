18ff04a55d8ead209ce58373bbaae415e477a5cb
fndribeiro <fndribeiro@hotmail.com>
2020-07-02 02:03:26 -0300

    add cors to main; removed required from racaPet.

--- src/main/java/br/com/cadastrodepets/CatsApplication.java
+++ src/main/java/br/com/cadastrodepets/CatsApplication.java
@@ -2,10 +2,15 @@ package br.com.cadastrodepets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class CatsApplication {
 
+	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CatsApplication.class, args);
 	}
--- src/main/java/br/com/cadastrodepets/pet/service/PetService.java
+++ src/main/java/br/com/cadastrodepets/pet/service/PetService.java
@@ -10,11 +10,9 @@ import org.springframework.stereotype.Service;
 import br.com.cadastrodepets.ong.model.Ong;
 import br.com.cadastrodepets.ong.repository.OngRepository;
 import br.com.cadastrodepets.pet.model.Pet;
-import br.com.cadastrodepets.pet.model.RacaPet;
 import br.com.cadastrodepets.pet.model.StatusPet;
 import br.com.cadastrodepets.pet.model.TipoPet;
 import br.com.cadastrodepets.pet.repository.PetRepository;
-import br.com.cadastrodepets.pet.repository.RacaPetRepository;
 import br.com.cadastrodepets.pet.repository.StatusPetRepository;
 import br.com.cadastrodepets.pet.repository.TipoPetRepository;
 
@@ -26,9 +24,6 @@ public class PetService {
 
 	@Autowired
 	private TipoPetRepository repositorioTipoPet;
-
-	@Autowired
-	private RacaPetRepository repositorioRacaPet;
 
 	@Autowired
 	private StatusPetRepository repositorioStatusPet;
@@ -43,9 +38,6 @@ public class PetService {
 
 		Optional<TipoPet> tipoPet = repositorioTipoPet.findById(pet.getTipoPet().getId());
 		pet.setTipoPet(tipoPet.get());
-
-		Optional<RacaPet> racaPet = repositorioRacaPet.findById(pet.getRacaPet().getId());
-		pet.setRacaPet(racaPet.get());
 
 		Optional<StatusPet> statusPet = repositorioStatusPet.findById(pet.getStatusPet().getId());
 		pet.setStatusPet(statusPet.get());
